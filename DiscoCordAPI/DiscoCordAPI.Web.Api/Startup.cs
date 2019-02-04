b6258b5e3ef151f51219f8020713da3463c40d7f
matixk <matixk73@gmail.com>
2019-02-04 19:46:36 +0100

    Added MS SQL local Db connection

--- DiscoCordAPI/DiscoCordAPI.Web.Api/Startup.cs
+++ DiscoCordAPI/DiscoCordAPI.Web.Api/Startup.cs
@@ -1,13 +1,28 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using DiscoCordAPI.Models.Context;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DiscoCordAPI.Web.Api
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("ApplicationContext")));
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -16,6 +31,8 @@ namespace DiscoCordAPI.Web.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvc();
         }
     }
 }
