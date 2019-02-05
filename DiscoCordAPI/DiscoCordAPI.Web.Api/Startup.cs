ec7ab844b1d513a337c11b52d639393f28b4f1b9
Tomasz Fijalkowski <tomelecf@gmail.com>
2019-02-05 14:17:56 +0100

    Added Authentication Service in Startup

--- DiscoCordAPI/DiscoCordAPI.Web.Api/Startup.cs
+++ DiscoCordAPI/DiscoCordAPI.Web.Api/Startup.cs
@@ -1,10 +1,14 @@
-﻿using DiscoCordAPI.Models.Context;
+﻿using System.Text;
+using DiscoCordAPI.Models.Context;
+using DiscoCordAPI.Web.Api.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DiscoCordAPI.Web.Api
 {
@@ -22,14 +26,26 @@ namespace DiscoCordAPI.Web.Api
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddDbContext<ApplicationContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("ApplicationContext")));
+
+            services.AddScoped<IAuthRepository, AuthRepository>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                            .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
             app.UseMvc();
         }
