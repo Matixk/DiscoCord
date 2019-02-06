157d4f3e3ae551421b2391e041c42c0a42f1ae6b
Tomasz Fijalkowski <tomelecf@gmail.com>
2019-02-06 14:49:55 +0100

    Added ServerForUserListDto

---
+++ DiscoCordAPI/DiscoCordAPI.Models/Servers/ServerForUserListDto.cs
@@ -0,0 +1,8 @@
+namespace DiscoCordAPI.Models.Servers
+{
+    public class ServerForUserListDto
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; }
+    }
+}
