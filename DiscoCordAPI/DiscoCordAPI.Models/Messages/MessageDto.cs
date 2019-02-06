653c84744d1d6fcf6273138bed09e8af4887ca84
empyty <bartoszlistwan@gmail.com>
2019-02-06 15:18:56 +0100

    Limited data shown with a message

---
+++ DiscoCordAPI/DiscoCordAPI.Models/Messages/MessageDto.cs
@@ -0,0 +1,11 @@
+﻿using DiscoCordAPI.Models.Channels;
+
+namespace DiscoCordAPI.Models.Messages
+{
+    class MessageDto
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public virtual ChannelForMessageDto Channel { get; set; }
+    }
+}
