b35f18aeb3a1eb936d3b3759109c02d931c17778
matixk <matixk73@gmail.com>
2019-02-06 13:48:03 +0100

    IRepository added

---
+++ DiscoCordAPI/DiscoCordAPI.Web.Api/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace DiscoCordAPI.Web.Api.Repositories
+{
+    public interface IRepository<T>
+    {
+        List<T> GetAll();
+        T Get(long id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
