204f1747528fc025c8792ac37f128190816045d6
empyty <bartoszlistwan@gmail.com>
2019-02-06 15:06:08 +0100

    Added dto for channel data that comes with a message

---
+++ DiscoCordAPI/DiscoCordAPI.Models/Channels/ChannelForMessageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DiscoCordAPI.Models.Channels
+{
+    public class ChannelForMessageDto
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; }
+    }
+}
