8a0320590980452c46b261d4a8f3ef21a21b0ed0
matixk <matixk73@gmail.com>
2019-02-06 13:47:52 +0100

    Base generic repository class to extend added

---
+++ DiscoCordAPI/DiscoCordAPI.Web.Api/Repositories/Repository.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using DiscoCordAPI.Models;
+using DiscoCordAPI.Models.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscoCordAPI.Web.Api.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : Entity
+    {
+        private readonly ApplicationContext context;
+        private DbSet<T> entities;
+
+        public Repository(ApplicationContext context)
+        {
+            this.context = context;
+            entities = context.Set<T>();
+        }
+
+        public List<T> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Get(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Insert(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
