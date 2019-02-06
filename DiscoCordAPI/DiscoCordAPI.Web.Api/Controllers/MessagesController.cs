ac28e3a9aea68ae8932af9c2dd3ad829572bca04
empyty <bartoszlistwan@gmail.com>
2019-02-06 11:53:32 +0100

    Add Messages Controller

---
+++ DiscoCordAPI/DiscoCordAPI.Web.Api/Controllers/MessagesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DiscoCordAPI.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        // GET: /<controller>/
+    }
+}
