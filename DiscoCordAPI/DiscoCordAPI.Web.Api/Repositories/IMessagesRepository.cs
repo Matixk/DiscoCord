bbc97d9b757e7ec644183a0711dea49ec668885f
empyty <bartoszlistwan@gmail.com>
2019-02-06 11:58:38 +0100

    Add message repository interface

---
+++ DiscoCordAPI/DiscoCordAPI.Web.Api/Repositories/IMessagesRepository.cs
@@ -0,0 +1,17 @@
+﻿using DiscoCordAPI.Models.Messages;
+using DiscoCordAPI.Models.Users;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace DiscoCordAPI.Web.Api.Repositories
+{
+    interface IMessagesRepository
+    {
+        Task<Message> GetMessageById(int id);
+        Task<Message> SendMessage(Channel channel, User author, string content);
+        Task<Message> UpdateMessage(string content);
+        Task<Message> DeleteMessage(int id);
+        Task<Message> MessageExists(int id);
+        Task<Message> UserIsTheAuthor(User author, int id);
+    }
+}
