d632adc4e7e4e269392952626a18d57f086fd753
matixk <matixk73@gmail.com>
2019-02-07 11:40:29 +0100

    ServerDtos added

---
+++ DiscoCordAPI/DiscoCordAPI.Models/Servers/ServerForUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiscoCordAPI.Models.Servers
+{
+    public class ServerForUpdateDto
+    {
+        [Required] [MaxLength(50)] public string Name { get; set; }
+
+        [Required] public bool IsPrivate { get; set; }
+    }
+}
---
+++ DiscoCordAPI/DiscoCordAPI.Models/Servers/ServerPreviewDto.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using DiscoCordAPI.Models.Channels;
+using DiscoCordAPI.Models.Users;
+
+namespace DiscoCordAPI.Models.Servers.Dto
+{
+    public class ServerPreviewDto
+    {
+        public int Id { get; private set; }
+
+        public User Owner { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsPrivate { get; set; }
+
+        public virtual List<BasicPreviewDto> Users { get; set; }
+        public virtual List<BasicPreviewDto> Channels { get; set; }
+    }
+}
