8bf014bb0050eefb953510bbf37b2d0ea8cf457f
empyty <bartoszlistwan@gmail.com>
2019-02-07 10:05:27 +0100

    Added get messages for channel method

--- DiscoCordAPI/DiscoCordAPI.Web.Api/Repositories/MessagesRepository.cs
+++ DiscoCordAPI/DiscoCordAPI.Web.Api/Repositories/MessagesRepository.cs
@@ -1,6 +1,8 @@
 ﻿using DiscoCordAPI.Models.Context;
 using DiscoCordAPI.Models.Messages;
 using DiscoCordAPI.Models.Users;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -20,16 +22,26 @@ namespace DiscoCordAPI.Web.Api.Repositories
         {
             var message = await context.Messages.FirstOrDefaultAsync(msg => msg.Id == id);
             context.Messages.Remove(message);
+            await context.SaveChangesAsync();
         }
 
         public async Task<Message> GetMessageById(int id)
         {
-            return await context.Messages.FirstOrDefaultAsync(msg => msg.Id == id);
+            return await context.Messages.FindAsync(id);
         }
 
-        public Task<IEnumerable<Message>> GetMessagesForChannel(int id)
+        public async Task<IEnumerable<Message>> GetMessagesForChannel(int id)
         {
-            throw new System.NotImplementedException();
+            var allMessages = await context.Messages.ToListAsync();
+            var messages = new List<Message>();
+            foreach (var msg in allMessages)
+            {
+                if (msg.Channel.Id == id)
+                {
+                    messages.Add(msg);
+                }
+            }
+            return messages;
         }
 
         public async Task<bool> MessageExists(int id)
@@ -40,6 +52,7 @@ namespace DiscoCordAPI.Web.Api.Repositories
         public async void SendMessage(Message message)
         {
             await context.Messages.AddAsync(message);
+            await context.SaveChangesAsync();
         }
 
         public async void UpdateMessage(int id, string content)
