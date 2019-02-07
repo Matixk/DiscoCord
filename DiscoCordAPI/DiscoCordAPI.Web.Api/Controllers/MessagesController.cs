34fca6ab617963c6246de22935b657d9a062c9ba
empyty <bartoszlistwan@gmail.com>
2019-02-07 10:05:48 +0100

    Added endpoints for methods from message repository

--- DiscoCordAPI/DiscoCordAPI.Web.Api/Controllers/MessagesController.cs
+++ DiscoCordAPI/DiscoCordAPI.Web.Api/Controllers/MessagesController.cs
@@ -1,6 +1,12 @@
-﻿using DiscoCordAPI.Web.Api.Repositories;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using DiscoCordAPI.Models.Context;
+using DiscoCordAPI.Models.Messages;
 
 namespace DiscoCordAPI.Web.Api.Controllers
 {
@@ -8,13 +14,93 @@ namespace DiscoCordAPI.Web.Api.Controllers
     [ApiController]
     public class MessagesController : ControllerBase
     {
-        private readonly IConfiguration config;
-        private readonly IMessagesRepository repo;
+        private readonly ApplicationContext _context;
 
-        public MessagesController(IConfiguration configuration, IMessagesRepository repository)
+        public MessagesController(ApplicationContext context)
         {
-            config = configuration;
-            repo = repository;
+            _context = context;
+        }
+
+        // GET: api/Messages
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Message>>> GetMessages()
+        {
+            return await _context.Messages.ToListAsync();
+        }
+
+        // GET: api/Messages/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Message>> GetMessage(int id)
+        {
+            var message = await _context.Messages.FindAsync(id);
+
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            return message;
+        }
+
+        // PUT: api/Messages/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMessage(int id, Message message)
+        {
+            if (id != message.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(message).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MessageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Messages
+        [HttpPost]
+        public async Task<ActionResult<Message>> PostMessage(Message message)
+        {
+            _context.Messages.Add(message);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMessage", new { id = message.Id }, message);
+        }
+
+        // DELETE: api/Messages/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Message>> DeleteMessage(int id)
+        {
+            var message = await _context.Messages.FindAsync(id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            _context.Messages.Remove(message);
+            await _context.SaveChangesAsync();
+
+            return message;
+        }
+
+        private bool MessageExists(int id)
+        {
+            return _context.Messages.Any(e => e.Id == id);
         }
     }
 }
