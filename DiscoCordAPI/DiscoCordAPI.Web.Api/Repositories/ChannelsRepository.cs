6b3a9ced05b2475c403c055c1525c9c9ae3d00ca
matixk <matixk73@gmail.com>
2019-02-19 10:24:03 +0100

    ChannelsRepository methods implemented

--- DiscoCordAPI/DiscoCordAPI.Web.Api/Repositories/ChannelsRepository.cs
+++ DiscoCordAPI/DiscoCordAPI.Web.Api/Repositories/ChannelsRepository.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using AutoMapper;
 using DiscoCordAPI.Models;
 using DiscoCordAPI.Models.Channels;
 using DiscoCordAPI.Models.Servers;
+using Microsoft.EntityFrameworkCore;
 
 namespace DiscoCordAPI.Web.Api.Repositories
 {
@@ -21,27 +20,44 @@ namespace DiscoCordAPI.Web.Api.Repositories
 
         public IEnumerable<BasicPreviewDto> GetChannelMessages(int id)
         {
-            throw new NotImplementedException();
+            var messages = context.GetDbSet()
+                .Include(c => c.Messages)
+                .FirstOrDefaultAsync(c => c.Id == id)
+                .Result
+                .Messages;
+
+            return mapper.Map<IEnumerable<BasicPreviewDto>>(messages);
         }
 
         public ChannelPreviewDto GetChannelDetails(int id)
         {
-            throw new NotImplementedException();
+            var channel = context.GetDbSet()
+                .Include(c => c.Messages)
+                .FirstOrDefaultAsync(c => c.Id == id)
+                .Result;
+
+            return mapper.Map<ChannelPreviewDto>(channel);
         }
 
         public void Insert(ChannelForCreateDto channel, Server server)
         {
-            throw new NotImplementedException();
+            var channelToCreate = new Channel(server, channel.Name);
+
+            context.Insert(channelToCreate);
         }
 
         public void Update(int id, ChannelForUpdateDto channel)
         {
-            throw new NotImplementedException();
+            var channelToUpdate= context.Get(id).Result;
+
+            channelToUpdate.Name = channel.Name;
+
+            context.Update(id, channelToUpdate);
         }
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            context.Delete(id);
         }
     }
 }
