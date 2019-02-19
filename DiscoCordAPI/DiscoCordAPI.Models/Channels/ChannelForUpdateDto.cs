5b811505645663e53742a0088f188979d051e6c5
matixk <matixk73@gmail.com>
2019-02-19 09:47:00 +0100

    Channel DTO models added

---
+++ DiscoCordAPI/DiscoCordAPI.Models/Channels/ChannelForUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiscoCordAPI.Models.Channels
+{
+    public class ChannelForUpdateDto
+    {
+        [Required] [MaxLength(50)] public string Name { get; set; }
+    }
+}
---
+++ DiscoCordAPI/DiscoCordAPI.Models/Channels/ChannelPreviewDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using DiscoCordAPI.Models.Messages;
+using DiscoCordAPI.Models.Servers;
+
+namespace DiscoCordAPI.Models.Channels
+{
+    public class ChannelPreviewDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Server Server { get; set; }
+
+        public virtual List<Message> Messages { get; set; } = new List<Message>();
+    }
+}
