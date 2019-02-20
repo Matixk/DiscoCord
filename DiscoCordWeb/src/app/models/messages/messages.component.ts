9e63ca577c2d84b4323dbe7a09ef95bf57bf7b3f
empyty <bartoszlistwan@gmail.com>
2019-02-20 15:17:20 +0100

    messages component takes channel id from active route, and subscribes to messages

--- DiscoCordWeb/src/app/models/messages/messages.component.ts
+++ DiscoCordWeb/src/app/models/messages/messages.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { IMessage } from './message';
+import { Component, OnInit } from "@angular/core";
+import { IMessage } from "./message";
+import { MessageService } from "./message.service";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-messages',
-  templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  selector: "app-messages",
+  templateUrl: "./messages.component.html",
+  styleUrls: ["./messages.component.css"]
 })
 export class MessagesComponent implements OnInit {
+  errorMessage: string;
   messages: IMessage[];
 
-  constructor() { }
+  constructor(private readonly messageService: MessageService,
+    private readonly route: ActivatedRoute) { }
 
   ngOnInit() {
+    const param = this.route.snapshot.paramMap.get("id");
+    if (param) {
+      const id = +param;
+      this.getMessages(id);
+    }
   }
 
+  getMessages(id: number) {
+    this.messageService.getMessages(id).subscribe(
+      messages => this.messages = messages,
+      error => this.errorMessage = error);
+  }
 }
