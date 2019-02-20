e6054082a9bb87f755838ab13b13c431a3770e00
empyty <bartoszlistwan@gmail.com>
2019-02-20 15:16:33 +0100

    Created message module

---
+++ DiscoCordWeb/src/app/models/messages/message.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { MessagesComponent } from "./messages.component";
+
+@NgModule({
+  declarations: [
+    MessagesComponent
+    ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      { path: "messages/:id", component: MessagesComponent }
+      ])
+  ]
+})
+export class MessageModule { }
