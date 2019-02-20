948795b72938920800d051db645d467bf29ea5bd
empyty <bartoszlistwan@gmail.com>
2019-02-20 15:16:17 +0100

    edited messages dto, so it shows the right content

--- DiscoCordAPI/DiscoCordAPI.Models/Messages/MessageDto.cs
+++ DiscoCordAPI/DiscoCordAPI.Models/Messages/MessageDto.cs
@@ -3,7 +3,9 @@
     public class MessageDto
     {
         public int Id { get; private set; }
-        public string Name { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+        public virtual BasicPreviewDto Author { get; set; }
         public virtual BasicPreviewDto Channel { get; set; }
     }
 }
