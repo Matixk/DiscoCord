197709a9d8ed86b3a64935d2191a80a5f19bd611
matixk <matixk73@gmail.com>
2019-02-28 16:55:09 +0100

    Added Material design form and router to shared module

--- DiscoCordWeb/src/app/shared/shared.module.ts
+++ DiscoCordWeb/src/app/shared/shared.module.ts
@@ -4,17 +4,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     exports: [
         MatIconModule,
         MatToolbarModule,
         MatListModule,
+        MatFormFieldModule,
         BrowserAnimationsModule,
         FormsModule,
         HttpClientModule,
+        RouterModule,
     ]
 })
 export class SharedModule { }
